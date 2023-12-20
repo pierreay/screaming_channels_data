730ff45c5f6ee7ef5ed7857126ef084c2cfc6c53
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-12-20 09:07:27 +0100

    Add bruteforce using HEL output in 231213

--- 231213_single-cable_raw/scripts/reproduce.sh
+++ 231213_single-cable_raw/scripts/reproduce.sh
@@ -1,3 +1,3 @@
 cd ~/git/screaming_channels_ble/src
 ./attack.py --plot --norm --dataset-path ~/storage/dataset/single_cable_raw --num-traces 44700 --start-point 73600 --end-point 74000 profile --pois-algo r --num-pois 2 --poi-spacing 2 --variable p_xor_k --align
-./attack.py --plot --norm --dataset-path ~/storage/dataset/single_cable_raw --num-traces 2500 --start-point 73600 --end-point 74000 attack --attack-algo pcc --num-pois 2 --poi-spacing 2 --variable p_xor_k --align
+./attack.py --plot --norm --dataset-path ~/storage/dataset/single_cable_raw --num-traces 2500 --start-point 73600 --end-point 74000 --bruteforce attack --attack-algo pcc --num-pois 2 --poi-spacing 2 --variable p_xor_k --align
