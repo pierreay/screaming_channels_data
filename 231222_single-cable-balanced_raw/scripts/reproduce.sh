17d8c4d69d3e42dd4cf048a6d53913618da72495
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-04 12:26:14 +0100

    Add last attack script and log results

--- 231222_single-cable-balanced_raw/scripts/reproduce.sh
+++ 231222_single-cable-balanced_raw/scripts/reproduce.sh
@@ -1,2 +1,4 @@
 # Profile:
 ./attack.py --plot --norm --dataset-path ~/storage/dataset/231222_single-cable-balanced_raw --num-traces 65536 --start-point 73950 --end-point 74350 profile --pois-algo r --num-pois 2 --poi-spacing 1 --variable p_xor_k --align
+# Attack:
+./attack.py --plot --norm --dataset-path ~/storage/dataset/231222_single-cable-balanced_raw --num-traces 4000 --start-point 73950 --end-point 74350 attack --attack-algo pcc --num-pois 2 --poi-spacing 1 --variable p_xor_k --align
