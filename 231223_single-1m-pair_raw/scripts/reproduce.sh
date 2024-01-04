f85d97086897e8e1595ec400469b9bb727b72415
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-04 13:13:57 +0100

    [231223_single-1m-pair_raw] attack using profile from previous dataset

---
+++ 231223_single-1m-pair_raw/scripts/reproduce.sh
@@ -0,0 +1,2 @@
+# Attack:
+./attack.py --plot --norm --dataset-path ~/storage/dataset/231223_single-1m-pair_raw --num-traces 32768 --start-point 73950 --end-point 74350 attack --profile ~/storage/dataset/231222_single-cable-balanced_raw/profile --attack-algo pcc --num-pois 2 --poi-spacing 1 --variable p_xor_k --align
