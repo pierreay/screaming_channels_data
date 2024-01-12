0eaf550dbca4c196d4bc7c104917c7abfd8266fd
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-01-12 13:27:09 +0100

    [240110_single-leak-pairing-1m-lna_raw] Attack last dataset

---
+++ 240110_single-leak-pairing-1m-lna_raw/scripts/reproduce.sh
@@ -0,0 +1,1 @@
+./attack.py --no-log --no-plot --norm --dataset-path /home/drac/storage/dataset/240110_single-leak-pairing-1m-lna_raw --start-point 73837 --end-point 74237 --num-traces 3987 attack --attack-algo pcc --profile /home/drac/git/screaming_channels_ble/data/profiles/231222_single-cable-balanced_raw/ --num-pois 2 --poi-spacing 1 --variable p_xor_k --align
