9500c4fd7dc32cc8417c1c17e82793f75fbdb132
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-02-10 11:53:53 +0100

    Revert "git-annex in dataset on Saren"
    This reverts commit 9f9d80010c8a6616f9201fb33bc02c8bc3bd8b2c.

--- 240126_300-leak-insub-10cm-lna_avg/scripts/profile.sh
+++ 240126_300-leak-insub-10cm-lna_avg/scripts/profile.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-dataset=$REPO_ROOT/240126_300-leak-insub-10cm-lna_avg
+dataset=$ENVRC_DATASET_PATH/240126_300-leak-insub-10cm-lna_avg
 
 # Profile with only 1 POI:
-$SC_SRC/attack.py --plot --norm --dataset-path $dataset --num-traces 0 --start-point 800 --end-point 1200 profile --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
-# mv ~/Figure_1.png $dataset/profile/plot_corr.png
-# mv ~/Figure_2.png $dataset/profile/plot_poi_1.png
-# mv $dataset/profile $dataset/profile_pois_1
+./attack.py --plot --norm --dataset-path $dataset --num-traces 0 --start-point 800 --end-point 1200 profile --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
+mv ~/Figure_1.png $dataset/profile/plot_corr.png
+mv ~/Figure_2.png $dataset/profile/plot_poi_1.png
+mv $dataset/profile $dataset/profile_pois_1
 
 # Profile with 2 POIs:
-# ./attack.py --plot --norm --dataset-path $dataset --num-traces 0 --start-point 800 --end-point 1200 profile --pois-algo r --num-pois 2 --poi-spacing 2 --variable p_xor_k --align
-# mv ~/Figure_1.png $dataset/profile/plot_corr.png
-# mv ~/Figure_2.png $dataset/profile/plot_poi_1.png
-# mv ~/Figure_3.png $dataset/profile/plot_poi_2.png
-# mv $dataset/profile $dataset/profile_pois_2
+./attack.py --plot --norm --dataset-path $dataset --num-traces 0 --start-point 800 --end-point 1200 profile --pois-algo r --num-pois 2 --poi-spacing 2 --variable p_xor_k --align
+mv ~/Figure_1.png $dataset/profile/plot_corr.png
+mv ~/Figure_2.png $dataset/profile/plot_poi_1.png
+mv ~/Figure_3.png $dataset/profile/plot_poi_2.png
+mv $dataset/profile $dataset/profile_pois_2
 
-# # Profile with 3 POIs:
-# ./attack.py --plot --norm --dataset-path $dataset --num-traces 0 --start-point 800 --end-point 1200 profile --pois-algo r --num-pois 3 --poi-spacing 1 --variable p_xor_k --align
-# mv ~/Figure_1.png $dataset/profile/plot_corr.png
-# mv ~/Figure_2.png $dataset/profile/plot_poi_1.png
-# mv ~/Figure_3.png $dataset/profile/plot_poi_2.png
-# mv ~/Figure_4.png $dataset/profile/plot_poi_3.png
-# mv $dataset/profile $dataset/profile_pois_3
+# Profile with 3 POIs:
+./attack.py --plot --norm --dataset-path $dataset --num-traces 0 --start-point 800 --end-point 1200 profile --pois-algo r --num-pois 3 --poi-spacing 1 --variable p_xor_k --align
+mv ~/Figure_1.png $dataset/profile/plot_corr.png
+mv ~/Figure_2.png $dataset/profile/plot_poi_1.png
+mv ~/Figure_3.png $dataset/profile/plot_poi_2.png
+mv ~/Figure_4.png $dataset/profile/plot_poi_3.png
+mv $dataset/profile $dataset/profile_pois_3
 
--- 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 dataset=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
-profile=$REPO_ROOT/240126_300-leak-insub-10cm-lna_avg/profile_pois_1
-profile_length=400
-start_point=1200
+profile=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/profile_AMPLITUDE
+profile_length=500
+start_point=1000
 end_point=$((start_point + profile_length))
-plot=--plot
+plot=--no-plot
 
 function attack() {
     trace_nb=$1
@@ -13,8 +13,8 @@ function attack() {
     $SC_SRC/attack.py --log $plot --norm --dataset-path $dataset --start-point $start_point --end-point $end_point --num-traces $trace_nb attack --attack-algo pcc --profile ${profile} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
 }
 
-# attack 500
-# attack 1000
-# attack 2000
-# attack 3000
+attack 500
+attack 1000
+attack 2000
+attack 3000
 attack 4000
