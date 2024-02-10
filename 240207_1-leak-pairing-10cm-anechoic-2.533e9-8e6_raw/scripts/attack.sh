e535fb70c989def153fb08d7e2c54f1f717c325f
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-02-10 10:15:17 +0100

    [240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw] Add last profile and attack

---
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dataset=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
+profile=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/profile_AMPLITUDE
+profile_length=500
+start_point=1000
+end_point=$((start_point + profile_length))
+plot=--no-plot
+
+function attack() {
+    trace_nb=$1
+    echo "Using $trace_nb traces with 1 POI:"
+    $SC_SRC/attack.py --log $plot --norm --dataset-path $dataset --start-point $start_point --end-point $end_point --num-traces $trace_nb attack --attack-algo pcc --profile ${profile} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
+}
+
+attack 500
+attack 1000
+attack 2000
+attack 3000
+attack 4000
--- 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/profile.sh
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/profile.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-dataset=$ENVRC_DATASET_PATH/230207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
-nt=6600
+dataset=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
+nt=16384
 sp=1000
 ep=1500
 
 function profile_comp() {
     comp=$1
     profile=profile_$comp
-    ./attack.py --plot --norm --dataset-path $dataset --num-traces $nt --start-point $sp --end-point $ep --comptype $comp profile --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
+    echo "[?] Save plots using 's' to Figure_1.png and Figure_2.png"
+    $SC_SRC/attack.py --plot --norm --dataset-path $dataset --num-traces $nt --start-point $sp --end-point $ep --comptype $comp profile --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
     mv $dataset/profile $dataset/$profile
     mv ~/Figure_1.png $dataset/$profile/plot_corr.png
     mv ~/Figure_2.png $dataset/$profile/plot_poi_1.png
