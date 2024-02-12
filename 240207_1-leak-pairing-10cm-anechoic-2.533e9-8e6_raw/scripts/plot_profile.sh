b8d722cc760cf35e3b541a1b59ba746b1a4f3a87
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-02-12 16:04:10 +0100

    [240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw] Add scripts to plot profiles

---
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/plot_profile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# * Configuration
+
+# Dataset path.
+DATASET=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
+
+SCRIPT_WD="$(dirname $(realpath $0))"
+
+# * Script
+
+function plot_profile_config() {
+    # Profile configuration.
+    PROFILE_CONFIG=$1
+    # Profile path.
+    PROFILE=$DATASET/profile_${PROFILE_CONFIG}
+    # Outfile plot path.
+    OUTFILE=$DATASET/plot/profile_${PROFILE_CONFIG}.pdf
+    python3 $SCRIPT_WD/plot_profile.py $PROFILE $OUTFILE
+}
+
+plot_profile_config AMPLITUDE_16384
+plot_profile_config PHASE_ROT_16384
