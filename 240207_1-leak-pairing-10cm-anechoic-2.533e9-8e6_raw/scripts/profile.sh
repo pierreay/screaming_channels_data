54a7a26b9ada8de4cf9ebd025e65c6306f26e20a
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-02-12 10:37:25 +0100

    [240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw] Allow new profile creation into script

--- 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/profile.sh
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/profile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 dataset=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
-nt=16384
 sp=1000
 ep=1500
 
 function profile_comp() {
     comp=$1
-    profile=profile_$comp
+    nt=$2
+    profile=profile_${comp}_${nt}
     echo "[?] Save plots using 's' to Figure_1.png and Figure_2.png"
     $SC_SRC/attack.py --plot --norm --dataset-path $dataset --num-traces $nt --start-point $sp --end-point $ep --comptype $comp profile --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
     mv $dataset/profile $dataset/$profile
@@ -16,6 +16,6 @@ function profile_comp() {
 }
 
 # Profile using amplitude:
-profile_comp AMPLITUDE
+profile_comp AMPLITUDE 16384
 # Profile using phase rotation:
-profile_comp PHASE_ROT
+profile_comp PHASE_ROT 16384
