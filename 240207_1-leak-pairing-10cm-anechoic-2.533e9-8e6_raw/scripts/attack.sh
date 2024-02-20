ed71e36b1a1bf7bc1a849eab40e444c65ea2fec2
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-02-20 13:24:40 +0100

    [240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw] Add new attack using phase rotation

--- 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
 dataset=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
-profile=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/profile_AMPLITUDE_16384
+
 profile_length=500
 start_point=1000
 end_point=$((start_point + profile_length))
-plot=--no-plot
+plot=--plot
 
 function attack() {
     trace_nb=$1
-    echo "Using $trace_nb traces with 1 POI:"
-    $SC_SRC/attack.py --log $plot --norm --dataset-path $dataset --start-point $start_point --end-point $end_point --num-traces $trace_nb $2 attack --attack-algo pcc --profile ${profile} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
+    bruteforce=$2
+    profile_path=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/profile_$3
+    comptype=$4
+    echo trace_nb=$trace_nb
+    echo bruteforce=$bruteforce
+    echo profile_path=$profile_path
+    echo comptype=$comptype
+    $SC_SRC/attack.py --log $plot --norm --dataset-path $dataset --start-point $start_point --end-point $end_point --num-traces $trace_nb $bruteforce --comptype $comptype \
+                      attack --attack-algo pcc --profile ${profile_path} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
 }
 
-#attack 500
-#attack 1000
-#attack 2000
-#attack 3000
-#attack 4000
-attack 14900 --bruteforce
+# DONE: Attack using amplitude:
+# attack 500 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
+# attack 1000 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
+# attack 2000 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
+# attack 3000 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
+# attack 4000 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
+# attack 14900 --bruteforce AMPLITUDE_16384 AMPLITUDE
+
+# DONE: Attack using phase rotation:
+# attack 16000 --no-bruteforce PHASE_ROT_65536 PHASE_ROT
