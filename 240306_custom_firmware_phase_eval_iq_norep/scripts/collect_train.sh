8c3474a2d64a577b37050b0f8d466acc658250c4
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-03-05 22:39:49 +0100

    [240306_custom_firmware_phase_eval_iq_norep] Init new dataset

---
+++ 240306_custom_firmware_phase_eval_iq_norep/scripts/collect_train.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# * Parameters
+
+# Logging level for Python.
+LOG_LEVEL=INFO
+# Number of traces.
+NUM_TRACES=16000
+# Temporary collection path.
+TARGET_PATH=$REPO_ROOT/240305_custom_firmware_phase_eval_iq/train
+
+# * Functions
+
+# ** Configuration
+
+function configure_param_json_escape_path() {
+    # 1. Substitute "/" to "\/".
+    # 2. Add '"' around string.
+    echo \"${1//\//\\/}\"
+}
+
+function configure_param_json() {
+    config_file="$1"
+    param_name="$2"
+    param_value="$3"
+    echo "$config_file: $param_name=$param_value"
+    sed -i "s/\"${param_name}\": .*,/\"${param_name}\": ${param_value},/g" "$config_file"
+}
+
+function configure_json_plot() {
+    export CONFIG_JSON_PATH_SRC=$SC_POC/experiments/config/example_collection_collect_plot.json
+    export CONFIG_JSON_PATH_DST=$TARGET_PATH/example_collection_collect_plot.json
+    cp $CONFIG_JSON_PATH_SRC $CONFIG_JSON_PATH_DST
+    configure_param_json $CONFIG_JSON_PATH_DST "trigger_threshold" "90e3"
+}
+
+function configure_json_collect() {
+    export CONFIG_JSON_PATH_SRC=$SC_POC/experiments/config/example_collection_collect_plot.json
+    export CONFIG_JSON_PATH_DST=$TARGET_PATH/example_collection_collect.json
+    cp $CONFIG_JSON_PATH_SRC $CONFIG_JSON_PATH_DST
+    configure_param_json $CONFIG_JSON_PATH_DST "trigger_threshold" "90e3"
+    configure_param_json $CONFIG_JSON_PATH_DST "num_points" "$NUM_TRACES"
+    configure_param_json $CONFIG_JSON_PATH_DST "fixed_key" "false"
+    configure_param_json $CONFIG_JSON_PATH_DST "template_name" "$(configure_param_json_escape_path $TARGET_PATH/template.npy)"
+}
+
+# ** Instrumentation
+
+function record() {
+    plot=$1
+    echo "plot=$plot"
+    
+    # Kill previously started radio server.
+    pkill radio.py
+
+    sudo ykushcmd -d a # power off all ykush device
+    sleep 2
+    sudo ykushcmd -u a # power on all ykush device
+    sleep 4
+
+    # Start SDR server.
+    # NOTE: Make sure the JSON config file is configured accordingly to the SDR server here.
+    $SC_SRC/radio.py --config $SC_SRC/config.toml --dir $HOME/storage/tmp --loglevel $LOG_LEVEL listen 128e6 2.512e9 8e6 --nf-id -1 --ff-id 0 --duration=0.6 --gain 76 &
+    sleep 10
+
+    # Start collection and plot result.
+    echo "Press 's' to save figs to ~/Figure_{1,2,3,4,5,6}.png"
+    sc-experiment --loglevel=$LOG_LEVEL --radio=USRP --device=$(nrfjprog --com | cut - -d " " -f 5) -o $HOME/storage/tmp/raw_0_0.npy collect $CONFIG_JSON_PATH_DST $TARGET_PATH $plot --average-out=$TARGET_PATH/template.npy
+    if [[ "$plot" == "--plot" ]]; then
+        mv ~/Figure_1.png $TARGET_PATH/plot_template_amp.png
+        mv ~/Figure_2.png $TARGET_PATH/plot_template_phr.png
+        mv ~/Figure_3.png $TARGET_PATH/plot_template_i.png
+        mv ~/Figure_4.png $TARGET_PATH/plot_template_q.png
+        mv ~/Figure_5.png $TARGET_PATH/plot_template_i_augmented.png
+        mv ~/Figure_6.png $TARGET_PATH/plot_template_q_augmented.png
+    fi
+}
+
+function analyze_only() {
+    sc-experiment --loglevel=$LOG_LEVEL --radio=USRP --device=$(nrfjprog --com | cut - -d " " -f 5) -o $HOME/storage/tmp/raw_0_0.npy extract $CONFIG_JSON_PATH_DST $TARGET_PATH --plot --average-out=$TARGET_PATH/template.npy
+}
+
+# * Script
+
+# Create collection directory.
+mkdir -p $TARGET_PATH
+
+# ** Configure the extraction / Template generation
+
+# Set the JSON configuration file for one recording analysis.
+configure_json_plot
+
+# DONE: Use this once to record a trace. 
+# record --plot
+# DONE: Once the recording is good, use this to configure the analysis.
+# analyze_only
+
+if [[ ! -f $TARGET_PATH/template.npy ]]; then
+    echo "Template has not been created! (no file at $TARGET_PATH/template.npy)"
+    exit 1
+fi
+
+# ** Collect
+
+# Set the JSON configuration file for collection.
+configure_json_collect
+
+# DONE: Collect a set of profile traces.
+# record --no-plot
---
+++ 240306_custom_firmware_phase_eval_iq_norep/scripts/template_attack.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# * Parameters
+
+# Path of dataset used to create the profile.
+TRAIN_SET=$REPO_ROOT/240305_custom_firmware_phase_eval_iq/train
+# Base path used to store the created profile.
+PROFILE_PATH_BASE=$TRAIN_SET/../profile
+# Path of dataset used to perform the attack.
+ATTACK_SET=$REPO_ROOT/240305_custom_firmware_phase_eval_iq/attack
+
+# Number of traces to use for profile creation.
+NUM_TRACES_PROFILE=3200
+# Number of traces to use for attack.
+NUM_TRACES_ATTACK=50
+# Delimiters.
+START_POINT=0
+END_POINT=0
+
+# * Functions
+
+function profile_comp() {
+    # Get parameters.
+    comp=$1
+    # Set global parameters.
+    export PROFILE_PATH=${PROFILE_PATH_BASE}_${comp}_${NUM_TRACES_PROFILE}
+    # Initialize directories.
+    mkdir -p $PROFILE_PATH
+
+    # Create the profile.
+    profile
+
+    # Check result.
+    if [[ ! -f $PROFILE_PATH/PROFILE_MEAN_TRACE.npy ]]; then
+        echo "Profile has not been created! (no file at $PROFILE_PATH/*.npy)"
+        exit 1
+    fi
+}
+
+function profile() {
+    echo "Press 's' to save figs to ~/Figure_1.png and ~/Figure_2.png"
+    sc-attack --plot --norm --data-path $TRAIN_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_PROFILE --comp $comp profile $PROFILE_PATH --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k
+    mv ~/Figure_1.png $PROFILE_PATH/plot_mean_trace.png
+    mv ~/Figure_2.png $PROFILE_PATH/plot_poi_1.png
+}
+
+function attack_comp() {
+    # Get parameters.
+    comp=$1
+    # Set global parameters.
+    export PROFILE_PATH=${PROFILE_PATH_BASE}_${comp}_${NUM_TRACES_PROFILE}
+
+    echo "Attack '$comp' with profile: $PROFILE_PATH"
+    
+    # Perform the attack.
+    attack
+}
+
+function attack() {
+    # NOTE: Sampling rate is hardcoded in collect_*.sh scripts.
+    fs=8e6
+    # bruteforce="--bruteforce"
+    # plot="--plot"
+    sc-attack $plot --norm --data-path $ATTACK_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_ATTACK $bruteforce --comp $comp \
+              attack $PROFILE_PATH --attack-algo pcc --variable p_xor_k --align --fs $fs
+}
+
+# * Script
+
+# ** Profiles
+
+# DONE: Profile the amplitude:
+# profile_comp amp
+
+# DONE: Profile the phase rotation:
+# profile_comp phr
+
+# DONE: Profile the I component:
+# profile_comp i
+
+# DONE: Profile the Q component:
+# profile_comp q
+
+# DONE: Profile the I augmented component:
+# profile_comp i_augmented
+
+# DONE: Profile the Q augmented component:
+# profile_comp q_augmented
+
+# ** Attacks
+
+# Attack using previously created templates.
+
+# DONE:
+# attack_comp amp
+# DONE:
+# attack_comp phr
+# DONE:
+# attack_comp i
+# DONE:
+# attack_comp q
+# DONE:
+# attack_comp i_augmented
+# DONE:
+# attack_comp q_augmented
