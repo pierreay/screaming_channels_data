2f242a7ee224c623371c1c3ae8a4a86f815125b8
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-03-07 12:53:01 +0100

    [240306_custom_firmware_phase_eval_iq_norep] Use multiple POI algo + use --save-images + use --align and --fs for profile creation

--- 240306_custom_firmware_phase_eval_iq_norep/scripts/template_attack.sh
+++ 240306_custom_firmware_phase_eval_iq_norep/scripts/template_attack.sh
@@ -10,26 +10,34 @@ PROFILE_PATH_BASE=$TRAIN_SET/../profile
 ATTACK_SET=$REPO_ROOT/240306_custom_firmware_phase_eval_iq_norep/attack
 
 # Number of traces to use for profile creation.
-NUM_TRACES_PROFILE=3200
+NUM_TRACES_PROFILE=16000
 # Number of traces to use for attack.
 NUM_TRACES_ATTACK=50
-# Delimiters.
+# Delimiters. Small window greatly increase profile computation speed.
 START_POINT=0
 END_POINT=0
+
+# NOTE: Sampling rate is hardcoded in collect_*.sh scripts.
+FS=8e6
 
 # * Functions
 
 function profile_comp() {
     # Get parameters.
     comp=$1
-    # Set global parameters.
-    export PROFILE_PATH=${PROFILE_PATH_BASE}_${comp}_${NUM_TRACES_PROFILE}
-    # Initialize directories.
-    mkdir -p $PROFILE_PATH
 
-    # Create the profile.
-    profile
-
+    # Iterate over good POI algorithms.
+    for pois_algo in snr t r corr 
+    do
+        echo "pois_algo=$pois_algo"
+        # Set global parameters.
+        export PROFILE_PATH=${PROFILE_PATH_BASE}_${comp}_${NUM_TRACES_PROFILE}_${pois_algo}
+        # Initialize directories.
+        mkdir -p $PROFILE_PATH
+        # Create the profile.
+        profile $pois_algo
+    done
+    
     # Check result.
     if [[ ! -f $PROFILE_PATH/PROFILE_MEAN_TRACE.npy ]]; then
         echo "Profile has not been created! (no file at $PROFILE_PATH/*.npy)"
@@ -38,10 +46,15 @@ function profile_comp() {
 }
 
 function profile() {
-    echo "Press 's' to save figs to ~/Figure_1.png and ~/Figure_2.png"
-    sc-attack --plot --norm --data-path $TRAIN_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_PROFILE --comp $comp profile $PROFILE_PATH --pois-algo r --num-pois 1 --poi-spacing 2 --variable p_xor_k
-    mv ~/Figure_1.png $PROFILE_PATH/plot_mean_trace.png
-    mv ~/Figure_2.png $PROFILE_PATH/plot_poi_1.png
+    # Get parameters.
+    pois_algo=$1
+    # Print options.
+    plot=--no-plot
+    echo "plot=$plot"
+    save_images=--save-images
+    echo "save_images=$save_images"
+    # Profile.
+    sc-attack $plot $save_images --norm --data-path $TRAIN_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_PROFILE --comp $comp profile $PROFILE_PATH --pois-algo $pois_algo --num-pois 1 --poi-spacing 2 --variable p_xor_k --align --fs $FS
 }
 
 function attack_comp() {
@@ -57,23 +70,21 @@ function attack_comp() {
 }
 
 function attack() {
-    # NOTE: Sampling rate is hardcoded in collect_*.sh scripts.
-    fs=8e6
     # bruteforce="--bruteforce"
     # plot="--plot"
     sc-attack $plot --norm --data-path $ATTACK_SET --start-point $START_POINT --end-point $END_POINT --num-traces $NUM_TRACES_ATTACK $bruteforce --comp $comp \
-              attack $PROFILE_PATH --attack-algo pcc --variable p_xor_k --align --fs $fs
+              attack $PROFILE_PATH --attack-algo pcc --variable p_xor_k --align --fs $FS
 }
 
 # * Script
 
 # ** Profiles
 
-# WAIT: Profile all available components:
-profile_comp amp
-profile_comp phr
-profile_comp i_augmented
-profile_comp q_augmented
+# DONE: Profile all available components:
+# profile_comp amp
+# profile_comp phr
+# profile_comp i_augmented
+# profile_comp q_augmented
 
 # ** Attacks
 
