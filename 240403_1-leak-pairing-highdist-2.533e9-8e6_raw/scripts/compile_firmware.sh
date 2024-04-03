49cb71d2d8cf5944f7ee7a930bb32828be60da62
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-03 17:27:02 +0200

    Turn compile_firmware.sh into function

--- 240403_1-leak-pairing-highdist-2.533e9-8e6_raw/scripts/compile_firmware.sh
+++ 240403_1-leak-pairing-highdist-2.533e9-8e6_raw/scripts/compile_firmware.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cd $SC_POC/firmware
-# git checkout 276e0c4
-direnv exec . make -C pca10040/blank/armgcc flash
+function compile_firmware() {
+    cd $SC_POC/firmware
+    # git checkout 276e0c4
+    direnv exec . make -C pca10040/blank/armgcc flash
+}
+
+# DONE:
+# compile_firmware
