789fdfc51bf65ee9b5a3cd8456a2e27cd6f69128
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-04 12:04:07 +0200

    Improve script for custom firmware calibration

---
+++ 240403_1-leak-pairing-highdist-2.533e9-8e6_raw/scripts/rf_calibration/calibrate_with_custom.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# * Functions
+
+function compile_flash_firmware() {
+    cd $SC_POC/firmware
+    # git checkout 276e0c4
+    direnv exec . make -C pca10040/blank/armgcc flash
+}
+
+function start_tx() {
+    script=/tmp/script.minicom
+    cat << EOF > $script
+send a
+send 02
+send o
+! killall -9 minicom
+EOF
+    minicom -D /dev/ttyACM0 -S $script >/dev/null 2>/dev/null &
+    
+}
+
+function stop_tx() {
+    script=/tmp/script.minicom
+    cat << EOF > $script
+send e
+! killall -9 minicom
+EOF
+    minicom -D /dev/ttyACM0 -S $script >/dev/null 2>/dev/null &
+}
+
+# * Steps
+
+# DONE:
+# compile_flash_firmware
+
+start_tx
+
+echo "INFO: Tune to 2.402 GHz and observe modulated carrier"
+gqrx
+
+stop_tx
