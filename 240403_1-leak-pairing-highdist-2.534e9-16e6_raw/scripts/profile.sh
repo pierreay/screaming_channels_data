46c022a06c6c895697ee09b33b3fbeb83e13e2e1
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-08 22:01:04 +0200

    Better profiling script (auto detect previous runs & save images)

--- 240403_1-leak-pairing-highdist-2.534e9-16e6_raw/scripts/profile.sh
+++ 240403_1-leak-pairing-highdist-2.534e9-16e6_raw/scripts/profile.sh
@@ -9,15 +9,19 @@ function profile_comp() {
     nt=$2
     pois_algo=$3
     profile=profile_${comp}_${nt}_${pois_algo}
-    # NOTE: Do not need to save plots as the "profile_plot.{sh,py}" scripts are
-    # here. TODO: Also save profile plots here because of the POIs values which
-    # are not handle in profile_plot.py.
-    $SC_SRC/attack.py --plot --norm --dataset-path ${DATASET} --num-traces ${nt} --start-point ${SP} --end-point ${EP} --comptype ${comp} profile --pois-algo ${pois_algo} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
+    if [[ -d "${DATASET}/${profile}" ]]; then
+        echo "[!] Profile already created: ${profile}"
+        return 0
+    fi
+    
+    plot=--no-plot
+    save_images=--save-images
+    $SC_SRC/attack.py ${plot} ${save_images} --norm --dataset-path ${DATASET} --num-traces ${nt} --start-point ${SP} --end-point ${EP} --comptype ${comp} profile --pois-algo ${pois_algo} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
     mv $DATASET/profile $DATASET/$profile
 }
 
 # DONE: Compare the impact of the number of traces for both components:
-# profile_comp AMPLITUDE 10000 r
-# profile_comp AMPLITUDE 19000 r
-# profile_comp PHASE_ROT 10000 r
-# profile_comp PHASE_ROT 19000 r
+profile_comp AMPLITUDE 10000 r
+profile_comp AMPLITUDE 19000 r
+profile_comp PHASE_ROT 10000 r
+profile_comp PHASE_ROT 19000 r
