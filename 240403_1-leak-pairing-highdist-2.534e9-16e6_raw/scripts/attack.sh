5630bb4e924a0a86203646aca43880cbadae7910
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-11 22:26:29 +0200

    Update attack script using variable number of POIs

--- 240403_1-leak-pairing-highdist-2.534e9-16e6_raw/scripts/attack.sh
+++ 240403_1-leak-pairing-highdist-2.534e9-16e6_raw/scripts/attack.sh
@@ -16,6 +16,7 @@ PLOT=--no-plot
 COMPARE_PNB=1
 COMPARE_ANB=1
 COMPARE_ALGO=1
+COMPARE_POINB=1
 COMPARE_COMP=1
 
 # * Functions
@@ -23,14 +24,16 @@ COMPARE_COMP=1
 function attack() {
     trace_nb=$1
     bruteforce=$2
-    profile_path=$DATASET/profile_$3
+    profile_path=$DATASET/profile_$3_$5
     comptype=$4
+    pois_nb=$5
     echo trace_nb=$trace_nb
     echo bruteforce=$bruteforce
     echo profile_path=$profile_path
     echo comptype=$comptype
+    echo pois_nb=$pois_nb
     $SC_SRC/attack.py --log $PLOT --norm --dataset-path $DATASET --start-point $START_POINT --end-point $END_POINT --num-traces $trace_nb $bruteforce \
-                      attack-recombined --comptype $comptype --attack-algo pcc --profile ${profile_path} --num-pois 1 --poi-spacing 1 --variable p_xor_k --align
+                      attack-recombined --comptype $comptype --attack-algo pcc --profile ${profile_path} --num-pois ${pois_nb} --poi-spacing 1 --variable p_xor_k --align
 }
 
 # * Script
@@ -48,28 +51,35 @@ mkdir -p "${DATASET}/logs"
 # Compare number of traces for profile:
 if [[ $COMPARE_PNB == 1 ]]; then
     for num_traces in 5000 10000 19000 30000; do
-        attack 3000 --no-bruteforce AMPLITUDE_${num_traces}_corr AMPLITUDE
+        attack 3000 --no-bruteforce AMPLITUDE_${num_traces}_corr AMPLITUDE 1
     done
 fi
 
 # Compare number of traces for attacks:
 if [[ $COMPARE_ANB == 1 ]]; then
     for num_traces in 1000 3000 7000 20000; do
-        attack ${num_traces} --no-bruteforce AMPLITUDE_10000_corr AMPLITUDE
+        attack ${num_traces} --no-bruteforce AMPLITUDE_10000_corr AMPLITUDE 1
     done
 fi
 
 # Compare POIS algorithm:
 if [[ $COMPARE_ALGO == 1 ]]; then
     for pois_algo in r snr corr; do
-        attack 3000 --no-bruteforce AMPLITUDE_10000_${pois_algo} AMPLITUDE
+        attack 3000 --no-bruteforce AMPLITUDE_10000_${pois_algo} AMPLITUDE 1
+    done
+fi
+
+# Compare POIS number:
+if [[ $COMPARE_POINB == 1 ]]; then
+    for pois_nb in 1 2 3; do
+        attack 3000 --no-bruteforce AMPLITUDE_10000_corr AMPLITUDE ${pois_nb}
     done
 fi
 
 # Compare components results (including recombination):
 if [[ $COMPARE_COMP == 1 ]]; then
     for comp in AMPLITUDE PHASE_ROT RECOMBIN; do
-        attack 3000 --no-bruteforce '{}_10000_corr' ${comp}
+        attack 3000 --no-bruteforce '{}_10000_corr' ${comp} 1
     done
 fi
 
