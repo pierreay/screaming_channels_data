b1dff0df42d911bc08aadb3d9a3e1dd28cc4c386
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-13 14:25:12 +0200

    Prepare a new collection

---
+++ 240413_1-leak-pairing-highdist-fix-2.534e9-8e6_raw/scripts/attack.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# * Variables
+
+# Paths.
+DATASET=$REPO_ROOT/240413_1-leak-pairing-highdist-fix-2.534e9-8e6_raw
+LOGFILE_PATH=${DATASET}/logs/attack.log
+
+# Parameters.
+PROFILE_LENGTH=1000
+START_POINT=2000
+END_POINT=$((START_POINT + PROFILE_LENGTH))
+PLOT=--no-plot
+
+# Actions.
+COMPARE_PNB=1
+COMPARE_ANB=1
+COMPARE_ALGO=1
+COMPARE_POINB=1
+COMPARE_COMP=1
+
+# * Functions
+
+function attack() {
+    trace_nb=$1
+    bruteforce=$2
+    profile_path=$DATASET/profile_$3_$5
+    comptype=$4
+    pois_nb=$5
+    echo trace_nb=$trace_nb
+    echo bruteforce=$bruteforce
+    echo profile_path=$profile_path
+    echo comptype=$comptype
+    echo pois_nb=$pois_nb
+    $SC_SRC/attack.py --log $PLOT --norm --dataset-path $DATASET --start-point $START_POINT --end-point $END_POINT --num-traces $trace_nb $bruteforce \
+                      attack-recombined --comptype $comptype --attack-algo pcc --profile ${profile_path} --num-pois ${pois_nb} --poi-spacing 1 --variable p_xor_k --align
+}
+
+# * Script
+
+if [[ -f ${LOGFILE_PATH} ]]; then
+    echo "[!] Attack had already be executed: ${LOGFILE_PATH}"
+    exit 0
+fi
+
+clear
+mkdir -p "${DATASET}/logs"
+
+(cd $SC && git checkout feat-recombination)
+
+num_traces_train_default=5000
+num_traces_attack_default=1000
+pois_algo_default=r
+pois_nb_default=1
+
+# Compare number of traces for profile:
+if [[ $COMPARE_PNB == 1 ]]; then
+    num_traces_train_list=(5000)
+    for num_traces_train in "${num_traces_train_list[@]}"; do
+        attack ${num_traces_attack_default} --no-bruteforce AMPLITUDE_${num_traces_train}_${pois_algo_default} AMPLITUDE ${pois_nb_default}
+    done
+fi
+
+# Compare number of traces for attacks:
+if [[ $COMPARE_ANB == 1 ]]; then
+    num_traces_attack_list=(100 300 500 700 1000)
+    for num_traces_attack in "${num_traces_attack_list[@]}"; do
+        attack ${num_traces_attack} --no-bruteforce AMPLITUDE_${num_traces_train_default}_${pois_algo_default} AMPLITUDE ${pois_nb_default}
+    done
+fi
+
+# Compare POIS algorithm:
+if [[ $COMPARE_ALGO == 1 ]]; then
+    pois_algo_list=(r snr)
+    for pois_algo in "${pois_algo_list[@]}"; do
+        attack ${num_traces_attack_default} --no-bruteforce AMPLITUDE_${num_traces_train_default}_${pois_algo} AMPLITUDE ${pois_nb_default}
+    done
+fi
+
+# Compare POIS number:
+if [[ $COMPARE_POINB == 1 ]]; then
+    pois_nb_list=(1 2)
+    for pois_nb in "${pois_nb_list[@]}"; do
+        attack ${num_traces_attack_default} --no-bruteforce AMPLITUDE_${num_traces_train_default}_${pois_algo_default} AMPLITUDE ${pois_nb}
+    done
+fi
+
+# Compare components results (including recombination):
+if [[ $COMPARE_COMP == 1 ]]; then
+    comp_list=(AMPLITUDE PHASE_ROT RECOMBIN)
+    for comp in "${comp_list[@]}"; do
+        attack ${num_traces_attack_default} --no-bruteforce '{}'"_${num_traces_train_default}_${pois_algo_default}" ${comp} ${pois_nb}
+    done
+fi
+
+tmux capture-pane -pS - > ${LOGFILE_PATH}
+
+grep -E "=|actual.rounded" "${LOGFILE_PATH}" > ${LOGFILE_PATH/.log/_summary.log}
---
+++ 240413_1-leak-pairing-highdist-fix-2.534e9-8e6_raw/scripts/collect.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# * Variables
+
+# ** Screaming Channels .envrc
+
+export ENVRC_VICTIM_PORT="$(nrfjprog --com | cut - -d " " -f 5)"
+export ENVRC_SAMP_RATE=08000000 # 8e6
+export ENVRC_DURATION=0.2
+export ENVRC_GAIN=53
+#export ENVRC_WANTED_TRACE_TRAIN=16384
+export ENVRC_WANTED_TRACE_TRAIN=5000
+#export ENVRC_WANTED_TRACE_ATTACK=16384
+export ENVRC_WANTED_TRACE_ATTACK=1500
+export ENVRC_NF_FREQ=128000000 # 128e6
+export ENVRC_FF_FREQ=2534000000 # 2.534e9
+export ENVRC_RADIO_DIR="$HOME/storage/tmp"
+export ENVRC_DATASET_PATH="$HOME/storage/dataset"
+export ENVRC_DATASET_RAW_PATH="$ENVRC_DATASET_PATH/240413_1-leak-pairing-highdist-fix-2.534e9-8e6_raw"
+export ENVRC_DATASET_AVG_PATH="$ENVRC_DATASET_PATH/tmp_avg"
+export ENVRC_DATASET_EXT_PATH="$ENVRC_DATASET_PATH/tmp_ext"
+export ENVRC_NIMBLE_PATH="$HOME/git/screaming_channels_nimble"
+export ENVRC_CONFIG_FILE="$ENVRC_DATASET_RAW_PATH/config.toml"
+export ENVRC_DURATION=0.1
+export ENVRC_VICTIM_ADDR="C2:3E:54:84:5C:4C"
+export ENVRC_ATTACKER_ADDR="00:19:0E:19:79:D8"
+export ENVRC_NF_ID=-1
+export ENVRC_FF_ID=0
+export ENVRC_EXTRACT_CONFIG="1_aes_ff_antenna_8msps"
+export ENVRC_DEVICE_CONFIG="fast"
+export ENVRC_DATASET_INPUT="PAIRING"
+
+# * Functions
+
+function init_config() {
+    rm -f $ENVRC_CONFIG_FILE
+    cp $SC_SRC/config.toml $ENVRC_CONFIG_FILE
+}
+
+# Example:
+# config "$ENVRC_CONFIG_FILE" "trg_bp_low" "[0.5e6]"
+function config() {
+    config_file="$1"
+    param_name="$2"
+    param_value="$3"
+    echo "${config_file}: ${param_name}=${param_value}"
+    sed -i "s/${param_name} = .*/${param_name} = ${param_value}/g" "${config_file}"
+}
+
+# * Script
+
+init_config
+config "$ENVRC_CONFIG_FILE" "accept_snr_min" "11.0"
+config "$ENVRC_CONFIG_FILE" "more_data_bit" "1"
+config "$ENVRC_CONFIG_FILE" "hop_interval" "15"
+config "$ENVRC_CONFIG_FILE" "procedure_interleaving" "false"
+config "$ENVRC_CONFIG_FILE" "ll_enc_req_conn_event" "4"
+# config "$ENVRC_CONFIG_FILE" "trg_bp_low" "${TRG_BP_LOW}"
+# config "$ENVRC_CONFIG_FILE" "trg_bp_high" "${TRG_BP_HIGH}"
+
+(cd $SC_SRC && ./collect.sh -l INFO -y)
