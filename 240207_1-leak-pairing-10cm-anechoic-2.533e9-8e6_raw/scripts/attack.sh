cace09496c029cc3c0262ec62c1610f55747f828
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-13 15:09:28 +0200

    Add new ad-hoc experiment code in script of anehcoic dataset

--- 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
+++ 240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/scripts/attack.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# * Variables
+
 dataset=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw
 
 profile_length=500
 start_point=1000
 end_point=$((start_point + profile_length))
 plot=--plot
+
+# * Functions
 
 function attack() {
     trace_nb=$1
@@ -20,6 +24,8 @@ function attack() {
                       attack --attack-algo pcc --profile ${profile_path} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
 }
 
+# * Script
+
 # DONE: Attack using amplitude:
 # attack 500 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
 # attack 1000 --no-bruteforce AMPLITUDE_16384 AMPLITUDE
@@ -30,3 +36,22 @@ function attack() {
 
 # DONE: Attack using phase rotation:
 # attack 16000 --no-bruteforce PHASE_ROT_65536 PHASE_ROT
+
+# * Ad-hoc experiment code
+
+# ** Validate recombination algorithm using previous dataset
+
+function attack() {
+    trace_nb=$1
+    bruteforce=$2
+    profile_path=$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw/profile_$3
+    comptype=$4
+    echo trace_nb=$trace_nb
+    echo bruteforce=$bruteforce
+    echo profile_path=$profile_path
+    echo comptype=$comptype
+    $SC_SRC/attack.py --log --no-plot --norm --dataset-path $dataset --start-point $start_point --end-point $end_point --num-traces $trace_nb $bruteforce \
+                      attack-recombined --comptype $comptype --attack-algo pcc --profile ${profile_path} --num-pois 1 --poi-spacing 2 --variable p_xor_k --align
+}
+
+attack 14900 --no-bruteforce '{}_16384' AMPLITUDE
