b00869c37bfb89a175005b89f8c59a9c9fe885cd
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-13 11:22:21 +0200

    Clean attack script from ad-hoc code

--- 240412_1-leak-pairing-highdist-fix-2.534e9-16e6_raw/scripts/attack.sh
+++ 240412_1-leak-pairing-highdist-fix-2.534e9-16e6_raw/scripts/attack.sh
@@ -18,7 +18,6 @@ COMPARE_ANB=1
 COMPARE_ALGO=1
 COMPARE_POINB=1
 COMPARE_COMP=1
-COMPARE_EXTPROF=0
 
 # * Functions
 
@@ -87,26 +86,3 @@ fi
 tmux capture-pane -pS - > ${LOGFILE_PATH}
 
 grep -E "=|actual.rounded" "${LOGFILE_PATH}" > ${LOGFILE_PATH/.log/_summary.log}
-
-# * Experimental ad-hoc code
-
-# ** Compare with external resampled profile
-
-# Compare with external profile:
-if [[ $COMPARE_EXTPROF == 1 ]]; then
-    # NOTE: This experiment implies to disable alignment with the profile in
-    # the repository code and align it visually.
-
-    echo "Internal profile:"
-    profile_in="$DATASET/profile_AMPLITUDE_10000_r_1"
-    $SC_SRC/attack.py --log --plot --norm --dataset-path $DATASET --start-point $START_POINT --end-point $END_POINT --num-traces 3000  --comptype AMPLITUDE \
-                      attack --attack-algo pcc --profile ${profile_in} --num-pois 1 --poi-spacing 1 --variable p_xor_k --align
-
-    echo "External profile:"
-    START_POINT=2133
-    END_POINT=$((START_POINT + PROFILE_LENGTH))
-    dataset_ext="$REPO_ROOT/240207_1-leak-pairing-10cm-anechoic-2.533e9-8e6_raw"
-    profile_ext="$dataset_ext/profile_AMPLITUDE_65536_resamp_16e6"
-    $SC_SRC/attack.py --log --plot --norm --dataset-path $DATASET --start-point $START_POINT --end-point $END_POINT --num-traces 3000  --comptype AMPLITUDE \
-                      attack --attack-algo pcc --profile ${profile_ext} --num-pois 1 --poi-spacing 1 --variable p_xor_k --align
-fi
