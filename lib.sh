72b652d9e8995f9ae35e420244d3583a0d876d4a
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-15 16:00:11 +0200

    Delete deprecated lib.sh containing overview()

--- lib.sh
+++
@@ -1,6 +0,0 @@
-# Display an overview of all datasets.
-function overview() {
-    tree -C -lh -L 3 -I "??_trace_ff.npy" -I "???_trace_ff.npy" -I "????_trace_ff.npy" -I "?????_trace_ff.npy" \
-                     -I "??_trace_nf.npy" -I "???_trace_nf.npy" -I "????_trace_nf.npy" -I "?????_trace_nf.npy" \
-                     . | less
-}
