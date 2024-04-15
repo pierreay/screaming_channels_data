1e5721272e4a945e13b0b1362d706e459b8d7c4f
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-15 16:42:30 +0200

    Minor

--- datasets/ble/240403_1-leak-pairing-highdist-2.534e9-16e6_raw/scripts/profile_plot.py
+++ datasets/ble/240403_1-leak-pairing-highdist-2.534e9-16e6_raw/scripts/profile_plot.py
@@ -10,6 +10,6 @@ OUTFILE_PDF=sys.argv[2]
 
 prof = dataset.Profile(fp=PROFILE_PATH)
 prof.load()
-# TODO: Temporary disable until LaTeX is installed on the current host.
+# XXX: Temporary disabled until LaTeX is installed on the current host.
 #libplot.enable_latex_fonts()
 prof.plot(save=OUTFILE_PDF, plt_param_dict={"lw": "0.5"})
