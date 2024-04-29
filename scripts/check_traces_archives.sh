e204cca9afada3b2e0c40bb60f3a9daa52848a5a
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-29 21:35:48 +0200

    Update general check_traces_archives.sh script for new ble datasets layout

--- scripts/check_traces_archives.sh
+++ scripts/check_traces_archives.sh
@@ -4,22 +4,31 @@
 
 VERBOSE=0
 
+function path_directory_to_archive() {
+    path="${1}"
+    printf "${path//\//_}.tar"
+}
+
 function check_datasets_under() {
     for dataset in $(realpath $(find "$1" -maxdepth 1 -type d -regex ".*23.*\|.*24.*")); do
         if [[ $VERBOSE == 1 ]]; then
             echo "INFO: Check: ${dataset}"
         fi
         # NOTE: List from .gitignore.
-        for directory in train train2 attack attack2; do
+        for directory in train train2 attack attack2 raw/train raw/attack avg/train avg/attack ext/train ext/attack; do
+            archive="$(path_directory_to_archive "${directory}")"
             if [[ -d "${dataset}/${directory}" ]]; then
                 size_run=$(stat -c "%s" "${dataset}/${directory}")
-                size_stage=$(stat -c '%s' "${dataset}/${directory}.tar")
+                size_stage=0
+                if [[ -f "${dataset}/${archive}" ]]; then
+                    size_stage=$(stat -c '%s' "${dataset}/${archive}")
+                fi
                 if [[ $VERBOSE == 1 ]]; then
                     echo size_run="$size_run"
                     echo size_stage="$size_stage"
                 fi
                 if [[ "$size_run" -gt "$size_stage" ]]; then
-                    echo "WARN: Re-compress ${directory} into ${directory}.tar: ${dataset}"
+                    echo "WARN: Please, compress ${directory} into ${archive}: ${dataset}"
                 fi
             fi
         done
