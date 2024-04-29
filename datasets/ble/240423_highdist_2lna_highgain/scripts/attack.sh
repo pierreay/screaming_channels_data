2226f47c0d839f561017b5863ca2a23f5ddf64c1
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-29 21:53:21 +0200

    Add ability to attack ext known as metaset attack

--- datasets/ble/240423_highdist_2lna_highgain/scripts/attack.sh
+++ datasets/ble/240423_highdist_2lna_highgain/scripts/attack.sh
@@ -32,13 +32,13 @@ readonly END_POINT=$((START_POINT + PROFILE_LENGTH))
 readonly PLOT=--plot
 # Number of traces for the attack.
 readonly NUM_TRACES_ATTACK_LIST=(200 700 2500 4000 10000 16000)
+# Paths.
+readonly METASET_LIST=(avg ext)
 
 # ** Internals
 
-# Paths.
-readonly DATASET="${DATASET_PATH}/avg"
 readonly LOG_PATH_BASE="${DATASET_PATH}/logs"
-readonly PROFILE_PATH_BASE="${DATASET}/profiles"
+readonly PROFILE_PATH_BASE="${DATASET_PATH}/avg/profiles"
 
 TMUX_PANE_CAPTURE=""
 
@@ -46,24 +46,26 @@ TMUX_PANE_CAPTURE=""
 
 function attack() {
     # Get parameters.
-    local num_traces="${1}"
-    local comp="${2}"
-    local pois_algo="${3}"
-    local pois_nb="${4}"
-    local num_traces_attack="${5}"
+    local metaset="${1}"
+    local num_traces="${2}"
+    local comp="${3}"
+    local pois_algo="${4}"
+    local pois_nb="${5}"
+    local num_traces_attack="${6}"
     # Set parameters.
+    local metaset_path="${DATASET_PATH}/${metaset}"
     local profile_path="${PROFILE_PATH_BASE}/${comp}_${num_traces}_${pois_algo}_${pois_nb}"
     if [[ "${comp}" == "RECOMBIN" ]]; then
         profile_path="${PROFILE_PATH_BASE}/"'{}'"_${num_traces}_${pois_algo}_${pois_nb}"
     fi
-    local log_path="${LOG_PATH_BASE}/attack_${comp}_${num_traces}_${pois_algo}_${pois_nb}_${num_traces_attack}.log"
+    local log_path="${LOG_PATH_BASE}/attack_${metaset}_${comp}_${num_traces}_${pois_algo}_${pois_nb}_${num_traces_attack}.log"
     local bruteforce="--no-bruteforce"
 
     # Safety-guard.
     if [[ -f "${log_path}" ]]; then
         echo "SKIP: Attack: File exists: ${log_path}"
         return 0
-    elif [[ $(ls -alh "${DATASET}/attack" | grep -E "*_trace_ff.npy" | wc -l) -lt "${num_traces_attack}" ]]; then
+    elif [[ $(ls -alh "${metaset_path}/attack" | grep -E "*_trace_ff.npy" | wc -l) -lt "${num_traces_attack}" ]]; then
         echo "SKIP: Attack: Not enough traces: < ${num_traces_attack}"
         return 0
     fi
@@ -71,7 +73,7 @@ function attack() {
     # Initialize log.
     mkdir -p "$LOG_PATH_BASE"
     # Perform the attack.
-    "${SC_SRC}"/attack.py --log "${PLOT}" --norm --dataset-path "${DATASET}" --start-point "${START_POINT}" --end-point "${END_POINT}" --num-traces "${num_traces_attack}" "${bruteforce}" \
+    "${SC_SRC}"/attack.py --log "${PLOT}" --norm --dataset-path "${metaset_path}" --start-point "${START_POINT}" --end-point "${END_POINT}" --num-traces "${num_traces_attack}" "${bruteforce}" \
                attack-recombined --comptype "${comp}" --attack-algo pcc --profile "${profile_path}" --num-pois "${pois_nb}" --poi-spacing 1 --variable p_xor_k --align
     # Finalize log.
     tmux capture-pane -t "${TMUX_PANE_CAPTURE}" -pS - > "${log_path}"
@@ -95,12 +97,14 @@ clear
 TMUX_PANE_CAPTURE="$(tmux-get-pane)"
 git_checkout main "${SC}"
 
-for comp in "${COMP_LIST[@]}"; do
-    for num_traces in "${NUM_TRACES_LIST[@]}"; do
-        for pois_algo in "${POIS_ALGO_LIST[@]}"; do
-            for pois_nb in "${POIS_NB_LIST[@]}"; do
-                for num_traces_attack in "${NUM_TRACES_ATTACK_LIST[@]}"; do
-                    attack "${num_traces}" "${comp}" "${pois_algo}" "${pois_nb}" "${num_traces_attack}"
+for metaset in "${METASET_LIST[@]}"; do
+    for comp in "${COMP_LIST[@]}"; do
+        for num_traces in "${NUM_TRACES_LIST[@]}"; do
+            for pois_algo in "${POIS_ALGO_LIST[@]}"; do
+                for pois_nb in "${POIS_NB_LIST[@]}"; do
+                    for num_traces_attack in "${NUM_TRACES_ATTACK_LIST[@]}"; do
+                        attack "${metaset}" "${num_traces}" "${comp}" "${pois_algo}" "${pois_nb}" "${num_traces_attack}"
+                    done
                 done
             done
         done
