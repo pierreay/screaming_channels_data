45da0c34e3fb5da7305b0574915db21c4b6ec71c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-05-15 21:33:59 +0200

    Add script for new expe

---
+++ experiments/2024-05-15_profile-icache-off/scripts/env.sh
@@ -0,0 +1,10 @@
+# Current dataset path.
+export DATASET_PATH="${REPO_EXPE_PATH}/2024-05-15_profile-icache-off"
+
+# Recording sampling rate.
+export FS="8e6"
+
+# Branch checked out in the "SC_POC" repository when compiling firmware.
+export SC_POC_BRCH_CHCKOUT_FIRMWARE="expe/source-flash-icache-off"
+# Branch checked out in the "SC_POC" repository when using utilities.
+export SC_POC_BRCH_CHCKOUT_CODE="feat-recombination-corr"
