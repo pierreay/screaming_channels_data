862bd4a338ab2a873d655df2a3ad6e402de781b4
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-05-17 11:43:24 +0200

    Add 2024-05-17_profile-reuse-1 experiment

---
+++ experiments/2024-05-17_profile-reuse-1/scripts/env.sh
@@ -0,0 +1,14 @@
+# Current dataset path.
+export DATASET_PATH="${REPO_DATASET_PATH}/ble/240429_highdist_2lna_highgain_norep"
+
+# Recording center frequency.
+export FC="2.533e9"
+
+# Recording sampling rate.
+export FS="8e6"
+
+# Recording gain [dB].
+export GAIN=70
+
+# Successfully sourced environment flag.
+export ENV_FLAG=1
