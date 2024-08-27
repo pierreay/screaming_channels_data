4d60769dcacbd15fe049dcde319bc6c4ae25a1b8
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-08-27 10:39:50 +0200

    Add profiles/signals from data dir. from screaming_channel_ble repo.

---
+++ profiles/231222_single-cable-balanced_raw/profile.sh
@@ -0,0 +1,1 @@
+./attack.py --plot --norm --dataset-path ~/storage/dataset/231222_single-cable-balanced_raw --num-traces 65536 --start-point 73950 --end-point 74350 profile --pois-algo r --num-pois 2 --poi-spacing 1 --variable p_xor_k --align
